747dec39fd3cc548b3d26ad1181a0b95e587953d
sanray66 <de1mo.s@mail.ru>
2025-04-07 21:05:55 +0500

    create paginator

--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 # myapp/views.py
 from itertools import product
 from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator
 from .models import Product, Category
 
 def popular_list(request):
@@ -20,16 +21,22 @@ def product_detail(request, slug):
 
 
 def product_list(request, category_slug=None):
+    page = request.GET.get('page', 1)
     category = None 
     categories = Category.objects.all()
     products = Product.objects.filter(available=True)
+    paginator = Paginator(products, 2)
+    current_page = paginator.page(int(page))
     if category_slug:
         category = get_object_or_404(Category,
                                      slug=category_slug)
+        paginator = Paginator(products.filter(category=category), 2)
         products = products.filter(category=category)
+        current_page = paginator.page(int(page))
     return render(request,
                   'main/product/list.html',
                   {'category': category,
                    'categories': categories,
-                   'products': products})
+                   'products': current_page,
+                   'slug_url': category_slug})
     
